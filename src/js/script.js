feceb0a2f8cb6ef99f271c3043ac1cce313a1b55
Dmitrij <dev.dfarino46@gmail.com>
2023-04-20 17:58:22 +0300

    Done!

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,23 @@
 window.addEventListener('scroll', function() {
     const scrolled = window.scrollY;
-    const target = document.querySelector('.nav');
+    const target = this.document.querySelector('.nav');
 
-    if (scrolled > 40) {
+    if (scrolled > 40 && this.document.documentElement.clientWidth >= 730) {
         target.classList.add('nav--scrolled');
     } else {
         target.classList.remove('nav--scrolled');
     }
 });
+
+
+function openMenu() {
+    const menu = document.querySelector('.menu')
+
+    menu.classList.add('menu--opened');
+}
+
+function closeMenu() {
+    const menu = document.querySelector('.menu')
+
+    menu.classList.remove('menu--opened');
+}
